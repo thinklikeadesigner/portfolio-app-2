ec3db92ae895f8ccfa9096f1b075401052d6fdeb
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-12 13:14:42 -0500

    Updates

---
+++ precache-manifest.80178224b5e0cef4761e1904ecd3818f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d094aa66e197ddeed33d31e38e1512",
+    "url": "/thinklikeadesigner/portfolio-app-2/index.html"
+  },
+  {
+    "revision": "b0a845418c9b92898540",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a845418c9b92898540",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/js/main.fa3a1945.chunk.js"
+  },
+  {
+    "revision": "7c680e776eb568cee978",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/js/runtime-main.6a7477f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thinklikeadesigner/portfolio-app-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
